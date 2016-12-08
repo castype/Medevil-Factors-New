655d6f7f613d0f4ea249b07fa5c0f2b12907b511
ari\dustin.melich <dustin.melich@ARI-DUSTIN-LT.ari.lcl>
2016-12-07 23:24:20 -0700

    launch catapult when engaged. reset when done

---
+++ Assets/Scripts/LaunchCatapult.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchCatapult : MonoBehaviour {
+
+    public float launchSpeedX;
+    public float launchSpeedY;
+    private Transform direction = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        SwingCatapult swingCatapult = gameObject.GetComponent<SwingCatapult>();
+        swingCatapult.activateR = true;
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        //Player_New.Instance.Jump = true;
+        //Player_New.Instance.MyRigidbody.AddForce(new Vector2(launchSpeedX * 10, launchSpeedY));
+
+        Player_New.Instance.MyRigidbody.AddForce(Vector3.up * 500);
+        Player_New.Instance.MyRigidbody.AddRelativeForce (Vector3.forward * 100);
+        
+    }
+}
---
+++ Assets/Scripts/SwingCatapult.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingCatapult : MonoBehaviour
+{
+    [SerializeField]
+    public float angle;
+
+    [SerializeField]
+    public float speed ;
+
+    public bool activateR;
+
+    public bool activateL;
+
+    Quaternion qStart, qEnd;
+
+    private float startTime;
+
+    private static bool swingEngaged;
+
+    void Start()
+    {
+        activateR = false;
+        activateL = false;
+        qStart = Quaternion.AngleAxis(-angle, Vector3.forward);
+        qEnd = Quaternion.AngleAxis(4.93f, Vector3.forward);
+        swingEngaged = false;
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (activateR == true)
+        {
+            startTime += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(qStart, qEnd, (Mathf.Sin(startTime * speed + Mathf.PI / 2) + 1.0f) / 2.0f);
+        }
+
+        if (transform.rotation.eulerAngles.z > 350)
+        {
+            swingEngaged = true;
+        }
+
+        if (transform.rotation.eulerAngles.z < 5f && swingEngaged && transform.rotation.eulerAngles.z > 4.4f)
+        {
+            activateR = false;
+            resetSwing();
+
+        }
+
+    }
+
+    void resetSwing()
+    {
+        startTime = 0.0f;
+        swingEngaged = false;
+        activateR = false;
+        activateL = false;
+        qStart = Quaternion.AngleAxis(-angle, Vector3.forward);
+        qEnd = Quaternion.AngleAxis(4.93f, Vector3.forward);
+        swingEngaged = false;
+    }
+
+    
+}
