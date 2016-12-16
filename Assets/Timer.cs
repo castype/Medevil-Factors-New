457970513690e774431edb55a229083b29d88762
ari\dustin.melich <dustin.melich@ARI-DUSTIN-LT.ari.lcl>
2016-12-16 15:26:46 -0700

    Remove test timer and add in real one

---
+++ Assets/Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text timerText;
+    private float startTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString("00");
+        string seconds = (t % 60).ToString("00");
+
+        timerText.text = "TIME " + minutes + ":" + seconds;
+
+
+    }
+}
